72dc157f73db29286ed119e4f7878df2b8e8cf85
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-10-28 22:15:26 -0400

    Add create action

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/photos.json')
+@app.route('/photos.json', )
 def index():
     return db.photos_all()
+
+@app.route("/photos.json", methods=["POST"])
+def create():
+    name = request.form.get("name")
+    width = request.form.get("width")
+    height = request.form.get("height")
+    return db.photos_create(name, width, height)
